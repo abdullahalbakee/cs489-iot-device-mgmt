d0667d59c4d6326cbdd76c0a3e56b9e614bab86a
Md. Abdullah-Al-Bakee <mabdullahalbakee@gmail.com>
2024-04-24 14:42:03 -0500

    Add dummy implementation for MeasurementService

---
+++ src/backend/src/main/java/edu/miu/cs489/cs489iotdevicemgmt/service/MeasurementServiceImpl.java
@@ -0,0 +1,19 @@
+package edu.miu.cs489.cs489iotdevicemgmt.service;
+
+import edu.miu.cs489.cs489iotdevicemgmt.dto.MeasurementDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MeasurementServiceImpl implements MeasurementService {
+    @Override
+    public List<MeasurementDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public MeasurementDto create(MeasurementDto device) {
+        return null;
+    }
+}
