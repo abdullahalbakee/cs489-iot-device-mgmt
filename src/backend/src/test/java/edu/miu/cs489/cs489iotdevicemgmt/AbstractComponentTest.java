682b63643729852c34acdf7d87b2a46ff2318a1d
Md. Abdullah-Al-Bakee <mabdullahalbakee@gmail.com>
2024-04-26 07:43:21 -0500

    Add unit tests

---
+++ src/backend/src/test/java/edu/miu/cs489/cs489iotdevicemgmt/AbstractComponentTest.java
@@ -0,0 +1,10 @@
+package edu.miu.cs489.cs489iotdevicemgmt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractComponentTest {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
---
+++ src/backend/src/test/java/edu/miu/cs489/cs489iotdevicemgmt/unit/MeasurementMapperTest.java
@@ -0,0 +1,33 @@
+package edu.miu.cs489.cs489iotdevicemgmt.unit;
+
+import edu.miu.cs489.cs489iotdevicemgmt.AbstractComponentTest;
+import edu.miu.cs489.cs489iotdevicemgmt.dto.MeasurementDto;
+import edu.miu.cs489.cs489iotdevicemgmt.mapper.MeasurementMapper;
+import edu.miu.cs489.cs489iotdevicemgmt.model.Measurement;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class MeasurementMapperTest extends AbstractComponentTest {
+    @Test
+    public void testMeasurement() {
+        var measurementDto = new MeasurementDto(null, 100.50);
+        var measurement = MeasurementMapper.toEntity(measurementDto);
+        assertThat(measurement.dateTime, is(nullValue()));
+        assertThat(measurement.value, closeTo(100.50, 0.001));
+    }
+
+    @Test
+    public void testMeasurement2() {
+        var measurement = Measurement.builder()
+                .dateTime(LocalDateTime.now())
+                .value(10.0)
+                .build();
+        var measurementDto = MeasurementMapper.toDto(measurement);
+        assertThat(measurementDto.dateTime(), is(notNullValue()));
+        assertThat(measurementDto.value(), closeTo(10.0, 0.001));
+    }
+}
